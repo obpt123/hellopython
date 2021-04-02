a06361a93111c1462e28c0946abba9e41bd701e6
obpt123 <obpt@sina.com>
2021-04-02 04:26:36 +0000

    rm useless code

--- testhello.py
+++ testhello.py
@@ -27,6 +27,6 @@ class TestDemo(unittest.TestCase):
 
 
 
-if __name__ == '__main__':
-    # verbosity=*：默认是1；设为0，则不输出每一个用例的执行结果；2-输出详细的执行结果
-    unittest.main(verbosity=1)
+# if __name__ == '__main__':
+#     # verbosity=*：默认是1；设为0，则不输出每一个用例的执行结果；2-输出详细的执行结果
+#     unittest.main(verbosity=1)
