aa9ee5e2131195ae3d7eb8ef9b26ac3217f85ab7
obpt123 <obpt@sina.com>
2021-04-02 04:21:14 +0000

    add unit test

--- hello.py
+++ hello.py
@@ -3,5 +3,8 @@
 def print_hello():
     print("hello")
 
+def add(a,b):
+    return a+b
+
 if __name__ == "__main__":
     print_hello()
---
+++ testhello.py
@@ -0,0 +1,32 @@
+import unittest
+
+import hello
+
+class TestDemo(unittest.TestCase):
+    """Test mathfuc.py"""
+
+    @classmethod
+    def setUpClass(cls):
+        print ("this setupclass() method only called once.\n")
+
+    @classmethod
+    def tearDownClass(cls):
+        print ("this teardownclass() method only called once too.\n")
+
+    def setUp(self):
+        print ("do something before test : prepare environment.\n")
+
+    def tearDown(self):
+        print ("do something after test : clean up.\n")
+
+    def test_add(self):
+        """Test method add(a, b)"""
+        print("hello")
+        self.assertEqual(3, hello.add(1, 2))
+       
+
+
+
+if __name__ == '__main__':
+    # verbosity=*：默认是1；设为0，则不输出每一个用例的执行结果；2-输出详细的执行结果
+    unittest.main(verbosity=1)
